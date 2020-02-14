9f208140c462698b69ebe45716b901ab16f9d266
Marty McCluskey <martymccluskey@martys-macbook-air.local>
2020-02-14 10:48:32 -0500

    Initial Commit

---
+++ DripNDash/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DripNDash
+//
+//  Created by Marty McCluskey on 2/14/20.
+//  Copyright © 2020 Liam McCluskey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ DripNDashTests/DripNDashTests.swift
@@ -0,0 +1,34 @@
+//
+//  DripNDashTests.swift
+//  DripNDashTests
+//
+//  Created by Marty McCluskey on 2/14/20.
+//  Copyright © 2020 Liam McCluskey. All rights reserved.
+//
+
+import XCTest
+@testable import DripNDash
+
+class DripNDashTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
